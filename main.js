f10504ce04cc176cf1fbddf2c9de186cc1f50112
Nate Reeves <nreeves1268@gmail.com>
2016-07-30 17:03:03 -0400

    Getting Random Number

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log ('hello')
+var listOfSecretWords = ['Frodo', 'Sam', 'Gandalf'];
+
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var indexOfWord = getRandomIntInclusive (0,2);
+console.log(indexOfWord);
