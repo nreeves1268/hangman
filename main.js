5bf4cf93c1eaccaf8b0c6df73457f9ab83895965
Nate Reeves <nreeves1268@gmail.com>
2016-07-30 17:53:42 -0400

    Displaying Alphabet

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-var listOfSecretWords = ['Frodo', 'Sam', 'Gandalf'];
+var listOfSecretWords = ['Frodo', 'Sam', 'Gandalf', 'Merry', 'Pippin'];
 
 function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var indexOfWord = getRandomIntInclusive (0,2);
+var indexOfWord = getRandomIntInclusive (0,listOfSecretWords.length);
 var secretWord = listOfSecretWords[indexOfWord];
+
+var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+
+$(function() {
+  $('#allLetters').text(alphabet);
+});
